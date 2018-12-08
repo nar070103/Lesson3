111d5caac3d2df14637a5b102c6263d0b3e1bb42
Your Name <you@example.com>
2018-12-08 14:52:31 +0400

    Narek

--- index.js
+++ index.js
@@ -34,17 +34,10 @@
 var express = require("express");
 var app = express();
 
-app.get("/", function(req, res){
-   res.send("<h1>Hello world</h1>");
+app.get("/:search", function (req, res) {
+    var search = req.params.search;
+    res.redirect("http://google.com/search?q=" + search);
 });
-app.get("/name/:search", function(req, res){
-   var name = req.params.name;
-   var search = req.params.search;
-   res.send("<h1>Hello " + name +"</h1>");
-});
-app.get("/google", function(req, res){
-    res.redirect('http://google.com');
-});
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("True");
 });
